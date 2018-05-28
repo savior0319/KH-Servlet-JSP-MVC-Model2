7ffcf049e72e67f83a92f59841d5e83811b2f11a
AHJ <savior0319@gmail.com>
2018-05-28 21:21:04 +0900

    -

--- src/jsp/notice/model/dao/NoticeDao.java
+++ src/jsp/notice/model/dao/NoticeDao.java
@@ -27,6 +27,7 @@ public class NoticeDao {
 		}
 	}
 
+	// 전체 페이지 조회
 	public ArrayList<NoticeVo> getCurrentPage(Connection conn, int currentPage, int recordCountPerPage) {
 
 		ArrayList<NoticeVo> aList = new ArrayList<NoticeVo>();
@@ -168,37 +169,63 @@ public class NoticeDao {
 		// navi 구조 끝
 
 		StringBuilder sb = new StringBuilder();
-
+		sb.append("<ul class=\"pagination\">");
 		if (needPrev) {
-			sb.append("<a href='/notice?currentPage=" + (startNavi - 1) + "'> < </a>");
+			sb.append("<li class=\"page-item\"><a class=\"page-link\"href='/notice?currentPage=" + (startNavi - 1)
+					+ "'> 이전 </a></li>");
 
 			// sb.append("<a href='/notice?currentPage=" + (startNavi - 5) + "'> << </a>");
 			// 5 페이지 앞으로
 			// sb.append("<a href='/notice?currentPage=1'> <<< </a>");
 			// 1 페이지로
 		}
-
 		for (int i = startNavi; i <= endNavi; i++) {
-			if (i == currentPage) {
-				sb.append("<a href='/notice?currentPage=" + i + "'><B> " + i + " </B></a>");
-			} else {
-				sb.append("<a href='/notice?currentPage=" + i + "'> " + i + " </a>");
-			}
+
+			sb.append("<li class=\"page-item\"><a class=\"page-link\" href='/notice?currentPage=" + i + "'>" + i
+					+ " </a></li>");
+
 		}
 
 		if (needNext) { // 끝 페이지가 아니라면
-			sb.append("<a href='/notice?currentPage=" + (endNavi + 1) + "'> > </a>");
+			sb.append("<li class=\"page-item\"><a class=\"page-link\"href='/notice?currentPage=" + (endNavi + 1)
+					+ "'> 다음 </a></li>");
 		}
-
+		sb.append("</ul>");
 		return sb.toString();
 	}
 
+	// 메뉴바 부분
+	// StringBuilder sb = new StringBuilder();
+	//
+	// if (needPrev) {
+	// sb.append("<a href='/notice?currentPage=" + (startNavi - 1) + "'> < </a>");
+	//
+	// // sb.append("<a href='/notice?currentPage=" + (startNavi - 5) + "'> <<
+	// </a>");
+	// // 5 페이지 앞으로
+	// // sb.append("<a href='/notice?currentPage=1'> <<< </a>");
+	// // 1 페이지로
+	// }
+	//
+	// for (int i = startNavi; i <= endNavi; i++) {
+	// if (i == currentPage) {
+	// sb.append("<a href='/notice?currentPage=" + i + "'><B> " + i + " </B></a>");
+	// } else {
+	// sb.append("<a href='/notice?currentPage=" + i + "'> " + i + " </a>");
+	// }
+	// }
+	//
+	// if (needNext) { // 끝 페이지가 아니라면
+	// sb.append("<a href='/notice?currentPage=" + (endNavi + 1) + "'> > </a>");
+	// }
+	//
+	// return sb.toString();
+
+	// 제목으로 검색
 	public String noticeSearchTitle(Connection conn, int currentPage, int recordCountPerPage, int naviCountPerPage,
 			String title) {
 
-		// 게시물의 총 개수를 구해야 함
-
-		int recordTotalCount = 0; // 총 게시물 개수 저장 변수 (정보가 없으므로 초기 값 0)
+		int recordTotalCount = 0;
 
 		String query = prop.getProperty("getSearchTitleContents");
 
@@ -211,7 +238,7 @@ public class NoticeDao {
 			if (rs.next()) {
 				recordTotalCount = rs.getInt("totalcount");
 			}
-			
+
 			System.out.println();
 
 		} catch (SQLException e) {
@@ -221,22 +248,12 @@ public class NoticeDao {
 			JDBCTemplate.close(pstmt);
 		}
 
-		// navi 총 개수
-
 		int pageTotalCount = 0;
 
-		// 페이지당 10개씩 보이게 만들어서 navi list를 만들려면
-		// 총 게시물이 124개 라고 했을 경우 124 % 10 한 후 + 1 만큼 만들어야 함
-		// 만약 나머지가 0이면 +1을 하지 않음
-
-		// 124 % 10
 		if (recordTotalCount % recordCountPerPage != 0) {
 			pageTotalCount = recordTotalCount / recordCountPerPage + 1;
 		} else
 			pageTotalCount = recordTotalCount / recordCountPerPage;
-
-		// 1페이지 에서 아래 페이지를 조회 할 경우 1페이지로 설정
-		// 마지막 페이지에서 다음 페이지를 조회 할 경우 마지막 페이지로 설정
 
 		if (currentPage < 1) {
 			currentPage = 1;
@@ -244,45 +261,16 @@ public class NoticeDao {
 			currentPage = pageTotalCount;
 		}
 
-		// 현재 페이지를 기점으로 시작 navi와 끝 navi를 만들어야 함
-		// 현재 페이지가 1이라면 -> 1 - 5
-		// 현재 페이지가 3이라면 -> 1 - 5
-		// 현재 페이지가 7이라면 -> 6 - 10
-		// 현재 페이지가 12이라면 -> 11 - 15
-		// ((현재페이지 - 1) / 리스트개수) * 리스트개수 + 1
-
-		// 만약 1페이지 라면
-		// ((1 - 1) / 5 ) * 5 + 1 -> 시작 페이지는 1
-		// 만약 3페이지 라면
-		// ((3 - 1) / 5 ) * 5 + 1 -> 시작 페이지는 1
-		// 만약 7페이지 라면
-		// ((7 - 1) / 5 ) * 5 + 1 -> 시작 페이지는 6
-		// 만약 12페이지 라면
-		// ((12 - 1) / 5) * 5 + 1 -> 시작 페이지는 11
-
 		int startNavi = ((currentPage - 1) / naviCountPerPage) * naviCountPerPage + 1;
 
-		// 끝 navi = 시작 navi + 보여줄 navi 개수 - 1;
-		// 만약 시작 navi가 1이라면
-		// 1 + 5 -1 -> 5
-
 		int endNavi = startNavi + naviCountPerPage - 1;
-
-		// 끝 navi를 구할 때 주의 해야 할 점
-		// 총 navi가 122 라고 가정 할 때 (현재 페이지는 121이라고 가정)
-		// 시작 navi -> ((121 - 1) / 5 ) * 5 + 1 = 121
-		// 끝 navi -> 121 + 5 - 1 = 125 -> 122까지 있어야 함
-		// 마지막 navi 부분은 아래 코드를 추가
 
 		if (endNavi > pageTotalCount) {
 			endNavi = pageTotalCount;
 		}
 
-		// 페이지 navi에서 사용할 '<', '>' 모양을 사용하기 위해 필요한 변수 2개 생성 (시작과 끝은 필요없음)
-
 		boolean needPrev = true, needNext = true;
 
-		// 둘다 속하는 경우 때문에 if if
 		if (startNavi == 1) {
 			needPrev = false;
 		}
@@ -291,17 +279,10 @@ public class NoticeDao {
 			needNext = false;
 		}
 
-		// navi 구조 끝
-
 		StringBuilder sb = new StringBuilder();
 
 		if (needPrev) {
 			sb.append("<a href='/notice?currentPage=" + (startNavi - 1) + "'> < </a>");
-
-			// sb.append("<a href='/notice?currentPage=" + (startNavi - 5) + "'> << </a>");
-			// 5 페이지 앞으로
-			// sb.append("<a href='/notice?currentPage=1'> <<< </a>");
-			// 1 페이지로
 		}
 
 		for (int i = startNavi; i <= endNavi; i++) {
@@ -311,8 +292,8 @@ public class NoticeDao {
 				sb.append("<a href='/notice?currentPage=" + i + "'> " + i + " </a>");
 			}
 		}
-
-		if (needNext) { // 끝 페이지가 아니라면
+		
+		if (needNext) {
 			sb.append("<a href='/notice?currentPage=" + (endNavi + 1) + "'> > </a>");
 		}
 
